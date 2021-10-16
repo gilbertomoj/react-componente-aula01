b629426206631d39d4364af2fb40f7dfffefb399
gilbertomoj <gibamedeirosgc@gmail.com>
2021-10-16 00:23:20 -0300

    Atividade

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Mensagem from "./Mensagem";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Mensagem />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mensagem.js
@@ -0,0 +1,14 @@
+function Mensagem() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+             Esta é uma mensagem do componente
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Mensagem;
+  
